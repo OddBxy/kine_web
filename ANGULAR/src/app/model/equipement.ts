6204a81426b73b303690c681a38ca4c6cf81560c
OddBxy <118909861+OddBxy@users.noreply.github.com>
2023-05-06 13:20:59 +0200

    Delete ANGULAR directory

--- ANGULAR/src/app/model/equipement.ts
+++
@@ -1,6 +0,0 @@
-export interface Equipement {
-    nom: string,
-    descriptif : string,
-    utilisation : string
-    image_source : string
-}
--- ANGULAR/src/app/model/pathologie.ts
+++
@@ -1,7 +0,0 @@
-export interface Pathologie {
-    nom: string;
-    descriptif: string;
-    resum_descriptif: string;
-    maladie: string;
-    image_source : string;
-}
