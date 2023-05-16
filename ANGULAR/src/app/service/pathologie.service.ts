72e3811affa778be19973c92bd21c66892eda2dc
OddBxy <118909861+OddBxy@users.noreply.github.com>
2023-05-16 09:17:19 +0200

    Update pathologie.service.ts

--- ANGULAR/src/app/service/pathologie.service.ts
+++ ANGULAR/src/app/service/pathologie.service.ts
@@ -8,23 +8,23 @@ export class PathologieService {
 
   static pathologies : Array<Pathologie> = [
     {
-      nom : "Pathologies Neurologiques",
-      descriptif : "Touche le système nerveux, central ou périphérique. Elle peut donc affecter des cellules nerveuses se trouvant dans le cerveau, la moelle épinière, les nerfs périphériques, les jonctions neuromusculaires, le système nerveux autonome... Le kiné prend en charge l'hémiplégique post AVC, la paraplégie, la sclérose en plaque, les IMC, ...",
-      resum_descriptif: "Troubles du système nerveux central ou périphérique dû à un accident ou une pathologie",
+      nom : "Pathologie Neurologique",
+      descriptif : "Touche le système nerveux, central ou périphérique. Elle peut donc affecter des cellules nerveuses se trouvant dans le cerveau, la moelle épinière, les nerfs périphériques, les jonctions neuromusculaires, le système nerveux autonome...",
+      resum_descriptif: "Type de trouble du système nerveux qui affecte le cerveau et les neurones",
       maladie: "Post AVC, hémiplégies, paraplègies, syndrome cerebelleux, imc, sclérose en plaque",
       image_source : "/assets/Neurologique.jpg"
     },
     {
-      nom : "Pathologies Rhumatologiques",
-      descriptif : "Ce sont des troubles du système osseux et cartilegien du corps du l'âge et aux différentes contraintes physiques. Le kiné va donc essayer d'améliorer l'état des patients atteint d'arthrose, d'arthrite, de spondylarthrite ankylosante, du rhumatisme articulaire aigü, de lombalgie chronique",
-      resum_descriptif: "Les pathologies rhumatologiques sont des problèmes articulaires, osseus et musculaires. Ces pathologies sont donc très variées, selon l’âge et le type d’activité pratiquée.",
+      nom : "Pathologie Rhumatologique",
+      descriptif : "Spécialité qui traite des pathologies articulaires, osseuses et musculaires : les rhumatismes. Ces pathologies sont donc très variées, selon l’âge et le type d’activité pratiquée.",
+      resum_descriptif: "Affecte les articulations, les os et les muscles",
       maladie: "Arthrose, arthrite, spondylarthrite ankylosante, lombalgie chronique",
       image_source : "/assets/Rhumatologique.jpg"
     },
     {
-      nom : "Pathologies Orthopédiques",
-      descriptif : "Troubles de la statique osseuse et musculaire causés par des traumatismes exterieures tel que des blessures exterieurs, des malformations congénitales, des problèmes de croissance .... Dans ces cas le kiné va aider à récuperer des amplitudes de mouvement après des fractures, des tendinites et autres.",
-      resum_descriptif: "Troubles qui affectent le système musculo-squelettique, pouvant affecter différentes parties du corps, notamment la colonne vertébrale, les membres supérieurs et inférieurs",
+      nom : "Pathologie Orthopédique",
+      descriptif : "Spécialité chirurgicale qui permet de corriger les défauts de l’appareil locomoteur de nature congénitale ou acquise durant la croissance voire à l’âge adulte.",
+      resum_descriptif: "Affecte l’ensemble du squelette osseux",
       maladie: "Scoliose, fracture, tendinopathie, prothèse",
       image_source : "/assets/Orthopedique.jpg"
     }
@@ -32,7 +32,16 @@ export class PathologieService {
 
   constructor() { }
 
+  i=0;
   static getPathologies() {
     return this.pathologies
+  }
+
+  setIndex(index: number){
+    this.i = index;
+  }
+
+  getIndex(){
+    return this.i
   }
 }
